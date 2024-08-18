680175e66a8b3f2e830e07d2346f52d80b89495d
Mustash <Mustash999@gmail.com>
2024-08-18 16:08:43 +0200

    yaqlla

--- MSS_ElementsID_syncronizer/src/main.py
+++ MSS_ElementsID_syncronizer/src/main.py
@@ -25,7 +25,7 @@ acu = conn.utilities
 # internal imports
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import opennings_control as op
 import table_control as table
